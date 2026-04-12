fcd2aa1453ab2465225a4ad9a3345c2701c18276
asari kazuyuki <orange.craft.sparkling@gmail.com>
2026-04-12 11:16:32 +0900

    バリデーション機能作成

--- src/resources/views/inquiry.blade.php
+++ src/resources/views/inquiry.blade.php
@@ -9,7 +9,7 @@
             <div class="contact-form__heading">
                 <h2>お問い合わせ</h2>
             </div>
-            <form class="form" action="/contacts/confirm" method="post">
+            <form class="form" action="/contacts/confirm" method="post" novalidate>
                 @csrf
                 <div class="form__group">
                     <div class="form__group-title">
@@ -18,10 +18,12 @@
                     </div>
                     <div class="form__group-content">
                         <div class="form__input--text">
-                            <input type="text" name="name" placeholder="テスト太郎" />
+                            <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
                         </div>
                         <div class="form__error">
-                            <!--バリデーション機能を実装したら記述します。-->
+                            @error('name')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -32,10 +34,12 @@
                     </div>
                     <div class="form__group-content">
                         <div class="form__input--text">
-                            <input type="email" name="email" placeholder="test@example.com" />
+                            <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
                         </div>
                         <div class="form__error">
-                            <!--バリデーション機能を実装したら記述します。-->
+                            @error('email')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -46,10 +50,12 @@
                     </div>
                     <div class="form__group-content">
                         <div class="form__input--text">
-                            <input type="tel" name="tel" placeholder="09012345678" />
+                            <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}" />
                         </div>
                         <div class="form__error">
-                            <!--バリデーション機能を実装したら記述します。-->
+                            @error('tel')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
